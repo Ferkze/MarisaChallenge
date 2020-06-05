c9488293f2254d04758d9426d2967931436750b8
Ferkze <ferkemartins@gmail.com>
2020-06-05 11:02:55 -0300

    Adicionado arquivos base do desafio marisa

---
+++ MarisaChallenge/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		System.out.println(sumPrimes(numbers));
+	}
+
+	public static int sumPrimes(int[] numbers) {
+
+		return 0;
+	}
+}
