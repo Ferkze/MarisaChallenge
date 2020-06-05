0a5a5e30941064b7ad795af7bc04af06c482f8e5
Ferkze <ferkemartins@gmail.com>
2020-06-05 11:21:32 -0300

    Adicionado checagem de números negativos

--- MarisaChallenge/src/Main.java
+++ MarisaChallenge/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] numbers = { -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 		System.out.println(sumPrimes(numbers));
 	}
 
@@ -17,6 +17,9 @@ public class Main {
 	}
 
 	public static boolean isPrime(int num) {
+		if (num < 1) {
+			return false;
+		}
 		for (int i = 2; i < num; i++) {
 			if (num % i == 0) {
 				return false;
